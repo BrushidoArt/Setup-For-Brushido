b770fd3b98040f03c0f1836dee4b5de20309068f
BrushidoArt <brushidoart@gmail.com>
2025-01-14 08:26:11 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -52,7 +52,8 @@ firefox \
   "https://openmodeldb.info/models/4x-Remacri" \
   "https://civitai.com/models/835655/illustrious-xl-personal-merge-noob-v-pred05-test-merge-updated" \
   "https://civitai.com/models/503815/nova-furry-xl?modelVersionId=1164762" \
-  "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111"
+  "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111" \
+  "http://127.0.0.1:7860/?__theme=dark"
 
 # Execute the script and wait for user input
 ./webui.sh

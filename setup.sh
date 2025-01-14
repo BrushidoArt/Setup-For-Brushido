4d06cbea0118debbf919d446574cc1b1c5dce239
BrushidoArt <brushidoart@gmail.com>
2025-01-14 09:17:46 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,18 +21,31 @@ sudo timedatectl set-timezone America/Chicago
 echo "Please enter a new password:"
 passwd
 
-# Navigate to Desktop directory and create a new directory
-cd ~/Ubuntu/Desktop/
-mkdir -p stablea1111
-cd stablea1111
-
 # Install necessary packages
 sudo apt install wget git python3 python3-venv libgl1 libglib2.0-0 -y
 wait
 
+# Navigate to Desktop directory and create a new directory
+cd ~/Desktop/
+mkdir -p stablea1111
+cd ~/Desktop/stablea1111/
+
 # Download the webui script from the GitHub repository
 wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh -O webui.sh
 wait
+
+# Modify permissions to make the script executable
+sudo chmod +x webui.sh
+
+# Execute the script and wait for user input
+./webui.sh
+wait
+
+# Navigate to Desktop directory and create a new directory
+cd ~/Desktop/stablea1111/stable-diffusion-webui
+
+# Modify the line in webui-user.sh to include --xformers --no-half-vae
+sed -i 's/^#export COMMANDLINE_ARGS=""/export COMMANDLINE_ARGS="--xformers --no-half-vae"/' webui-user.sh
 
 # Download the ui-config.json script from the GitHub repository
 wget -q  https://raw.githubusercontent.com/BrushidoArt/Setup-For-Brushido/master/ui-config.json -O ~/Desktop/stablea1111/stable-diffusion-webui/ui-config.json
@@ -42,15 +55,6 @@ wait
 wget -q  https://raw.githubusercontent.com/BrushidoArt/Setup-For-Brushido/master/config.json -O ~/Desktop/stablea1111/stable-diffusion-webui/config.json
 wait
 
-# Navigate into stable-diffusion-webui
-cd ~/Desktop/stablea1111/stable-diffusion-webui/
-
-# Modify the line in webui-user.sh to include --xformers --no-half-vae
-sed -i 's/^#export COMMANDLINE_ARGS=""/export COMMANDLINE_ARGS="--xformers --no-half-vae"/' webui-user.sh
-
-# Modify permissions to make the script executable
-sudo chmod +x webui.sh
-
 # Launch Firefox with the specified URLs
 firefox \
   "https://openmodeldb.info/models/4x-Remacri" \
@@ -58,12 +62,6 @@ firefox \
   "https://civitai.com/models/503815/nova-furry-xl?modelVersionId=1164762" \
   "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111" \
   "http://127.0.0.1:7860/?__theme=dark"
-
-# Navigate to Desktop directory and create a new directory
-cd ~/Ubuntu/Desktop/stablea1111
-
-# Execute the script and wait for user input
-./webui.sh
 
 # Final user interaction prompt
 echo "Please follow any additional on-screen instructions to proceed. Be sure to run the upscaler at least once to create the ESRGAN directory and download the items from Civitai and or the extensions section in the webui"
